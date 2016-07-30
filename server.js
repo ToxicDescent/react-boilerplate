030ed069f91763909e69d9dd2131e3aab0acb547
Patrick Haikal <pathaikal@gmail.com>
2016-07-30 18:22:13 +1000

    Initial commit.

---
+++ server.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const config = require('./webpack.config');
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+if (process.env.NODE_ENV !== 'production') {
+  const compiler = webpack(config);
+  const middleware = webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath,
+    stats: {
+      colors: true,
+      hash: false,
+      timings: true,
+      chunks: false,
+      chunkModules: false,
+      modules: false,
+    },
+  });
+
+  app.use(middleware);
+  app.use(webpackHotMiddleware(compiler));
+}
+
+app.use(express.static(path.join(__dirname + 'dist')));
+
+app.get('*', function response(request, response) {
+  response.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, '0.0.0.0', function onStart(error) {
+  if(error) {
+    console.error(error);
+  } else {
+    console.info('Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+  }
+});
